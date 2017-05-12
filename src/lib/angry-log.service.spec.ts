f2545b3d70a59c95b1cee7d186573baeca7bf0d8
Gabriel Sanches <gsanches.dev@gmail.com>
2017-05-11 22:14:27 -0300

    Fix indentation in spec

--- src/lib/angry-log.service.spec.ts
+++ src/lib/angry-log.service.spec.ts
@@ -27,25 +27,25 @@ describe("AngryLogService", (): void => {
         expect(ngls.URL).toEqual(TEST_URL);
     }));
 
-        it("should successfully POST the error log", inject([AngryLogService, XHRBackend], (ngls: AngryLogService, mockBackend: MockBackend): void => {
-            const ERR_MSG: string = "ERROR";
-            const mockResponse: { data: string } = {
-                data: ERR_MSG
-            };
+    it("should successfully POST the error log", inject([AngryLogService, XHRBackend], (ngls: AngryLogService, mockBackend: MockBackend): void => {
+        const ERR_MSG: string = "ERROR";
+        const mockResponse: { data: string } = {
+            data: ERR_MSG
+        };
 
-            mockBackend.connections.subscribe((connection: MockConnection) => {
-                connection.mockRespond(new Response(new ResponseOptions({
-                    body: JSON.stringify(mockResponse)
-                })));
+        mockBackend.connections.subscribe((connection: MockConnection) => {
+            connection.mockRespond(new Response(new ResponseOptions({
+                body: JSON.stringify(mockResponse)
+            })));
+        });
+
+        ngls.condition = false;
+        ngls.error(ERR_MSG)
+            .subscribe((response: { data: string }) => {
+                const MSG: string = response.data;
+
+                expect(MSG).toBeDefined();
+                expect(MSG).toEqual(ERR_MSG);
             });
-
-            ngls.condition = false;
-            ngls.error(ERR_MSG)
-                .subscribe((response: { data: string }) => {
-                    const MSG: string = response.data;
-
-                    expect(MSG).toBeDefined();
-                    expect(MSG).toEqual(ERR_MSG);
-                });
-        }));
+    }));
 });
